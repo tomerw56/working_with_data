90092ba846cb95f3a4e39d20e44a78e6af458bb4
tomerw56 <tomerw56@gmail.com>
2025-12-20 08:10:41 +0200

    added orange work

--- grafana_and_flux/export_to_csv.py
+++ grafana_and_flux/export_to_csv.py
@@ -1,4 +1,5 @@
 from influxdb_client import InfluxDBClient
+import pandas as pd
 
 client = InfluxDBClient(
     url="http://localhost:8086",
@@ -10,7 +11,14 @@ query = """
 from(bucket: "drone")
   |> range(start: -24h)
   |> filter(fn: (r) => r._measurement == "drone_telemetry")
+  |> filter(fn: (r) => r._field == "alt")
 """
 
 df = client.query_api().query_data_frame(query)
-print(df[0].head())
+
+# Clean up for Orange
+df = df[["_time", "drone_id", "_value"]]
+df.columns = ["time", "drone_id", "altitude"]
+
+df.to_csv("drone_altitude.csv", index=False)
+print("Exported drone_altitude.csv")
